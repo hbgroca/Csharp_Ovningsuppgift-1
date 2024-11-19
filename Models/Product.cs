843842b8012c02961c170920d5415191ebf503b6
Robin <156453367+hbgroca@users.noreply.github.com>
2024-11-19 09:41:59 +0100

    Update v2 klar

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖVNINGSUPPGIFT___SHOPPINGLISTA.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; } = 0;
+    }
+}
